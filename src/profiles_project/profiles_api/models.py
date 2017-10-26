2482e704599c53e36c5f2843bd075195cf038f51
STEVE KORNAHRENS <steve.kornahrens@gmail.com>
2017-10-25 20:28:54 -0600

    ebanle django admin for user profiles

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -15,23 +15,24 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError("Users must have an email address.")
 
-        email = self.normailize_email(email)
-        user = self.model(email=email, name=name)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, name, passowrd):
+    def create_superuser(self, email, name, password):
         """Creates and saves a new superuser with given details."""
 
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
-
         user.save(using=self._db)
+
+        return user
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Respents a "user profile" inside our system."""
